c0edf9946207940b601cfd6119ef6d6c0e95b79e
superLady <dpkaar@hotmail.com>
2019-12-06 17:30:27 -0600

    nothing

---
+++ src/hello/HiWorld.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class HiWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hi World!!");
+		System.out.println("I'm a part of this world");
+		
+
+	}
+
+}
